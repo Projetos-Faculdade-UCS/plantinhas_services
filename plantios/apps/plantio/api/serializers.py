e34fd76e3cc70ec3df2c09e9df8a20b357da15b9
Samuel Frederico <sfrederico@ucs.br>
2025-06-29 03:52:36 -0300

    refactor: :recycle: Refatora campos `SaudeField` e `SedeField` do `PlantioSerializer`
    * Implementa `SaudeField` para representar o estado de saúde da planta com base em um valor numérico. * Implementa `SedeField` para representar as necessidades hídricas da planta. * Ambos os campos incluem métodos para conversão de representação e validação de dados.

--- plantios/apps/plantio/api/serializers.py
+++ plantios/apps/plantio/api/serializers.py
@@ -1,9 +1,80 @@
+from typing import TypedDict
+
 from apps.plantio.models import Plantio
 
 from rest_framework import serializers
 
 
+class Indicador(TypedDict):
+    """TypedDict for Indicador field representation."""
+
+    label: str
+    value: float
+
+
+class SaudeField(serializers.Field):
+    """Custom field for handling health status of the plant."""
+
+    def to_representation(self, value: float) -> Indicador:
+        label = ""
+        if value >= 0.8:
+            label = "Ótima"
+        elif value >= 0.6:
+            label = "Boa"
+        elif value >= 0.4:
+            label = "Regular"
+        else:
+            label = "Ruim"
+        return {"label": label, "value": value}
+
+    def to_internal_value(self, data: Indicador | float) -> float:
+        try:
+            # If data is a dictionary (like from a PATCH with the output format)
+            print(type(data), data)
+            if isinstance(data, dict) and "value" in data:
+                return float(data["value"])
+            # If data is a simple value
+            return float(data)
+        except (ValueError, TypeError):
+            raise serializers.ValidationError(
+                "Saúde deve ser um valor numérico entre 0 e 1."
+            )
+
+
+class SedeField(serializers.Field):
+    """Custom field for handling the water needs of the plant."""
+
+    def to_representation(self, value: float) -> Indicador:
+        label = ""
+        if value >= 0.8:
+            label = "Alta"
+        elif value >= 0.6:
+            label = "Média"
+        elif value >= 0.4:
+            label = "Baixa"
+        else:
+            label = "Muito Baixa"
+        return {"label": label, "value": value}
+
+    def to_internal_value(self, data: Indicador | float) -> float:
+        try:
+            # If data is a dictionary (like from a PATCH with the output format)
+            if isinstance(data, dict) and "value" in data:
+                return float(data["value"])
+            # If data is a sim12ple value
+            return float(data)
+        except (ValueError, TypeError):
+            raise serializers.ValidationError(
+                "Sede deve ser um valor numérico entre 0 e 1."
+            )
+
+
 class PlantioSerializer(serializers.ModelSerializer):
+    """Serializer for Plantio model."""
+
+    saude = SaudeField()
+    sede = SedeField()
+
     class Meta:
         model = Plantio
         fields = [
