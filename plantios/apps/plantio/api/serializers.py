496dcd792facc40ac26c59f1d50992d34f59a728
Samuel Frederico <sfrederico@ucs.br>
2025-06-29 02:59:36 -0300

    feat(api): ✨ Atualiza `PlantioSerializer` para incluir o campo `id`
    * Adiciona o campo `id` ao serializer de Plantio.
    * Melhora a estrutura dos campos para refletir as informações adicionais necessárias.

--- plantios/apps/plantio/api/serializers.py
+++ plantios/apps/plantio/api/serializers.py
@@ -2,7 +2,17 @@ from apps.plantio.models import Plantio
 
 from rest_framework import serializers
 
+
 class PlantioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plantio
-        fields = ["planta_id", "user_id", "data_plantio", "data_prevista_colheita", "saude", "sede", "situacao"]
+        fields = [
+            "id",
+            "planta_id",
+            "situacao",
+            "saude",
+            "sede",
+            "data_plantio",
+            "data_prevista_colheita",
+            "informacoes_adicionais",
+        ]
