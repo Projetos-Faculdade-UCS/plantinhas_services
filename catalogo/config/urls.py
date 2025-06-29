7f5b504c666c0c5ead260254b03420f10db49a84
Marco Haubrich <marcolino.haubrich@gmail.com>
2025-06-28 22:52:44 -0300

    feat(api): ✨ Adiciona autenticação e configurações de rede no Docker
    * Habilita a autenticação nas views da API
    * Atualiza a configuração do Docker para incluir variáveis de ambiente do serviço de autenticação
    * Corrige a URL do banco de dados no Docker Compose
    * Organiza as rotas da API para incluir autenticação

--- catalogo/config/urls.py
+++ catalogo/config/urls.py
@@ -1,12 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", include("apps.core.urls")),
 ]
 
 # Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += (
+        path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    )
