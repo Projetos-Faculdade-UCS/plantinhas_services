300146261f9c1915f5d705be1ab5ede40c031288
Marco Haubrich <marcolino.haubrich@gmail.com>
2025-06-28 22:39:35 -0300

    feat(docker): ✨ Adiciona configuração do Docker para ambiente de desenvolvimento
    * Cria `Dockerfile.dev` para configurar o ambiente Python
    * Adiciona `docker-compose.yml` para orquestração de serviços
    * Define variáveis de ambiente para o serviço de autenticação
    * Configura o banco de dados PostgreSQL
    * Atualiza permissões do `manage.py` para execução
    * Ajusta dependências no `requirements/local.txt`

--- auth/apps/authentication/api/v1/views.py
+++ auth/apps/authentication/api/v1/views.py
@@ -49,6 +49,7 @@ class UserLoginView(APIView):
 
         if serializer.is_valid():
             auth_data = serializer.save()
+            print(auth_data)
             return Response(auth_data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
