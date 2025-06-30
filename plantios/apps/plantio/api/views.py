bd41113c75a500321fc967360aedd1f505634c04
Samuel Frederico <sfrederico@ucs.br>
2025-06-29 21:48:05 -0300

    feat(pagination): ✨ Adiciona classe `CustomPagination` para formatação de resposta paginada
    * Implementa uma classe de paginação personalizada que retorna dados de paginação em um formato específico.
    * Define parâmetros como `itensPorPagina`, `pagina` e `max_page_size`.
    * Inclui tratamento para casos em que não há páginas disponíveis.

--- plantios/apps/plantio/api/views.py
+++ plantios/apps/plantio/api/views.py
@@ -1,12 +1,12 @@
+from apps.plantio.api.pagination import CustomPagination
 from apps.plantio.api.serializers import PlantioSerializer
 from apps.plantio.models import Plantio
 
 from rest_framework import viewsets
 
 
-
 class PlantioViewSet(viewsets.ModelViewSet):
     queryset = Plantio.objects.all()
     serializer_class = PlantioSerializer
     http_method_names = ["get", "post", "put", "delete"]
-
+    pagination_class = CustomPagination
