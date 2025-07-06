ffe65cace009fa65337bf5325b1f122f029206fb
Bruno Martins <bgmartins@ucs.br>
2025-07-06 19:24:18 -0300

    feat(habilidade): ✨ Adiciona campo `descricao` ao modelo Habilidade
    * Atualiza o serializer `HabilidadeSerializer` para incluir o campo `descricao`
    * Modifica a migração inicial para adicionar o campo `descricao` ao modelo `Habilidade`
    * Cria arquivo de fixtures com dados de exemplo para o novo campo `descricao`

--- habilidades/apps/habilidade/models.py
+++ habilidades/apps/habilidade/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Habilidade(models.Model):
     nome: "models.CharField[str, str]" = models.CharField(max_length=50)
+    descricao: "models.TextField[str, str]" = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nome
